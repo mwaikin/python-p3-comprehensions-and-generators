ce2b97a67450cd92bb8d9734f3e27ce5c4d30d9b
mwaikin <mwaikingori10@gmail.com>
2024-05-22 10:24:37 +0300

    passed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    returns_evens = [n for n in num_list if n % 2 == 0]
+    return returns_evens
 def make_exclamation(sentence_list):
-    pass
+    make_exclamation = [n + "!" for n in sentence_list]
+    return make_exclamation
